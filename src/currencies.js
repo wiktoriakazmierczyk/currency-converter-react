c0b1f4cd630b0804ce42251f2f15968ee9439dda
Wiktoria Kazmierczyk <wiktoriakazmierczyk@protonmail.com>
2023-08-17 13:24:33 +0200

    currencies table added

---
+++ src/currencies.js
@@ -0,0 +1,31 @@
+export const currencies = [
+    {
+        id: 1,
+        name: "PLN (polski złoty)",
+        rate: 1.00,
+    },
+
+    {
+        id: 2,
+        name: "EUR (euro)",
+        rate: 4.73,
+    },
+
+    {
+        id: 3,
+        name: "GBP (funt brytyjski)",
+        rate: 5.37,
+    },
+
+    {
+        id: 4,
+        name: "CHF (frank szwajcarski)",
+        rate: 4.78,
+    },
+
+    {
+        id: 5,
+        name: "USD (dolar amerykański)",
+        rate: 4.46,
+    },
+];
